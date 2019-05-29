1201e6214a5bd62b8e2e2a3b93c3d425f1f1c1dd
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-05-29 11:47:03 +0700

    Constructor functions and the 'this' keyword

---
+++ constructor.js
@@ -0,0 +1,10 @@
+function User(firstName, lastName, age, gender) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender;
+}
+
+let user1 = new User('Dhimas', 'Akbar', 18, 'male');
+
+user1
