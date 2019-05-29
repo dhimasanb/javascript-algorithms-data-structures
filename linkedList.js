754850409e40f4aedf90c20abdb73956da9459a0
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-05-29 12:36:02 +0700

    Linked list and node constructor functions

---
+++ linkedList.js
@@ -0,0 +1,14 @@
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+}
+
+function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+
+let node1 = new Node(100, 'node2', null);
+
+console.log(node1);
