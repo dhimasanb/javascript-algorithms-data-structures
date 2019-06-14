96e2b7e51f5c9616cb9e3f822e7054cf548fb06d
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-06-14 20:56:30 +0700

    Reverse array in place algorithm

---
+++ Algorithm/reverseArrayInPlace.js
@@ -0,0 +1,11 @@
+function reverseArrayInPlace(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let tempVar = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = tempVar;
+  }
+
+  return arr;
+}
+
+reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7]);
