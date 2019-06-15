07de45b4210ded3bac831cd9f04532bb7e9a68fb
Dhimas Akbar Noor Baihaque <dhimasakbarnoorbaihaque@gmail.com>
2019-06-15 17:26:25 +0700

    Max stock profit algorithm

---
+++ Algorithm/maxStockProfit.js
@@ -0,0 +1,24 @@
+function maxStockProfit(pricesArr) {
+  let maxProfit = -1;
+  let buyPrice = 0;
+  let sellPrice = 0;
+
+  let changeBuyPrice = true;
+
+  for (let i = 0; i < pricesArr.length; i++) {
+    if (changeBuyPrice) buyPrice = pricesArr[i];
+    sellPrice = pricesArr[i + 1];
+
+    if (sellPrice < buyPrice) {
+      changeBuyPrice = true;
+    } else {
+      let tempProfit = sellPrice - buyPrice;
+      if (tempProfit > maxProfit) maxProfit = tempProfit;
+      changeBuyPrice = false;
+    }
+  }
+
+  return maxProfit;
+}
+
+maxStockProfit([10, 18, 4, 5, 9, 6, 16, 12]); // 12
